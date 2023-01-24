50d14d0cd3b647d180049764e7407f46370a831a
akmtzr <98877230+akmtzr@users.noreply.github.com>
2023-01-24 15:31:54 +0200

    Added some simple tests

--- Assets/MonetizrCampaigns/Tests/Tests.cs
+++ Assets/MonetizrCampaigns/Tests/Tests.cs
@@ -4,15 +4,21 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
-public class Tests
-{
-    // A Test behaves as an ordinary method
-    [Test]
-    public void TestsSimplePasses()
-    {
-        // Use the Assert class to test conditions
-    }
+using Monetizr.Campaigns;
 
+public class Tests
+{    
+    [Test]
+    public void UtilsTests()
+    {
+        Assert.AreEqual(Utils.CompareVersions("0.0.15", "0.0.14"), 1);
+        Assert.AreEqual(Utils.CompareVersions("0.0.11", "0.0.11"), 0);
+        Assert.AreEqual(Utils.CompareVersions("0.0.1", "0.0.11"), -1);
+
+        string ints = "1.2.3";
+        Assert.AreEqual(Utils.ConvertToIntArray(ints)[2], 3);
+    }
+    
     // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
     // `yield return null;` to skip a frame.
     [UnityTest]
