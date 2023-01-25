529fb9d009644fcc4ef353841d25aadc7388cbaf
Artem <ak@jetdogs.com>
2023-01-25 15:58:25 +0200

    Update test with some UI testing

--- Assets/MonetizrCampaigns/Tests/Tests.cs
+++ Assets/MonetizrCampaigns/Tests/Tests.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 using UnityEngine.TestTools;
 
 using Monetizr.Campaigns;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+using UnityEngine.UI;
 
 public class Tests
 {    
@@ -19,14 +22,46 @@ public class Tests
         Assert.AreEqual(Utils.ConvertToIntArray(ints)[2], 3);
     }
 
+    [SetUp]
+    public void Setup()
+    {
+        var go = new GameObject("Default Camera", typeof(Camera));
+        SceneManager.MoveGameObjectToScene(go, SceneManager.GetActiveScene());
+        go.transform.SetAsFirstSibling();
+
+    }
+
+    [TearDown]
+    public void Teardown()
+    {
+        
+    }
+
+    GameObject FindObjectByName(string name)
+    {
+        GameObject[] gos = (GameObject[])GameObject.FindObjectsOfType(typeof(GameObject));
+
+        for (int i = 0; i < gos.Length; i++)
+            if (gos[i].name.Contains(name))
+               return gos[i];
+
+        return null;
+    }
+
     [UnityTest]
     public IEnumerator SDKInitializationTest()
     {
+        PlayerPrefs.SetString("campaigns", "");
+        PlayerPrefs.SetString("missions", "");
+
+        bool isTestDone = false;
+
         int loadSuccess = 0;
 
         Sprite img = UnityEditor.AssetDatabase.GetBuiltinExtraResource<Sprite>("UI/Skin/UISprite.psd");
 
-        MonetizrManager.bundleId = "com.monetizer.sample";
+        MonetizrManager.bundleId = "com.monetizr.sample";
+                
 
         MonetizrManager.SetGameCoinAsset(RewardType.Coins, img, "Coins", 
             () => { return 0; },  
@@ -35,13 +70,15 @@ public class Tests
 
         MonetizrManager.Initialize("t_rsNjLXzbaWkJrXdvUVEc4IW2zppWyevl9j_S5Valo", null, () =>
         {
+            Assert.IsTrue(MonetizrManager.Instance.HasCampaignsAndActive(),"Campaign should have active campaigns");
+
             if (MonetizrManager.Instance.HasCampaignsAndActive())
             {
                 //we can show teaser manually, but better to use TeaserHelper script
                 //see DummyMainUI object in SampleScene
                 //dummyUI.SetActive(true);
 
-                //MonetizrManager.ShowTinyMenuTeaser();
+                MonetizrManager.ShowTinyMenuTeaser();
                 //Do something
 
                 loadSuccess = 1;
@@ -51,10 +88,36 @@ public class Tests
         while(loadSuccess == 0)
             yield return null;
 
+        //clicking teaser button
+
+        FindButtonAndClickIt("MonetizrMenuTeaser");
+
+        //closing RC
+
+        FindButtonAndClickIt("CloseButton");
+
+
+        //while (!isTestDone)
+        //    yield return null;
         //Assert.IsNotNull(o);
 
         Debug.Log("done");
 
-        yield return null;
+        //yield return null;
+    }
+
+    void FindButtonAndClickIt(string name)
+    {        
+        var buttonObject = FindObjectByName(name);
+
+        Assert.IsNotNull(buttonObject);
+
+        Button b = buttonObject.GetComponent<Button>();
+
+        Assert.IsNotNull(b);
+
+        b.onClick.Invoke();
+
+        Debug.Log("");
     }
 }
