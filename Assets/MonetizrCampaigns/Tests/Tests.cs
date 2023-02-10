5eedf3fc78e26b19fadd09ed13fe35dcb2b8f9a3
Artem <ak@jetdogs.com>
2023-02-10 17:33:05 +0200

    Additional tests update

--- Assets/MonetizrCampaigns/Tests/Tests.cs
+++ Assets/MonetizrCampaigns/Tests/Tests.cs
@@ -9,11 +9,14 @@ using UnityEngine.SceneManagement;
 using UnityEditor;
 using UnityEngine.UI;
 using System;
+using UnityEngine.UIElements;
+using Button = UnityEngine.UI.Button;
 
 public class Tests
 {
-    int loadSuccess = 0;
+    static int loadSuccess = 0;
     Dictionary<string, int> eventsAmount = new Dictionary<string, int>();
+    
 
     [Test]
     public void UtilsTests()
@@ -44,7 +47,7 @@ public class Tests
 
         MonetizrManager.claimForSkippedCampaigns = true;
 
-
+        eventsAmount.Clear();
 
         MonetizrManager.SetGameCoinAsset(RewardType.Coins, img, "Coins",
             () => { return 0; },
@@ -85,7 +88,7 @@ public class Tests
 
     }
 
-    GameObject FindObjectByName(string name)
+    static GameObject FindObjectByName(string name)
     {
         GameObject[] gos = (GameObject[])GameObject.FindObjectsOfType(typeof(GameObject));
 
@@ -95,89 +98,44 @@ public class Tests
 
         return null;
     }
-
-    private void UserDefinedEvent(string campaignId, string placement, MonetizrManager.EventType eventType)
-    {
-        throw new NotImplementedException();
-    }
-
+     
     [UnityTest]
     public IEnumerator SDKTest()
     {
-       
-        while(loadSuccess == 0)
-            yield return null;
-
         
-        yield return new WaitForSeconds(1);
-
-        FindButtonAndClickIt("MonetizrMenuTeaser");
-
-        yield return new WaitForSeconds(1);
         
-        FindButtonAndClickIt("RewardCenterCloseButton");
+        var tasks = new List<TestUITask>() {
+            new ClickTask("MonetizrMenuTeaser",2),
 
-        yield return new WaitForSeconds(1);
+            new ClickTask("RewardCenterCloseButton",3),
+            new ClickTask("MonetizrMenuTeaser",2),
+            new ClickTask("RewardCenterCloseButton",2),
+            new TeaserShowTask(false,1),
+            new TeaserShowTask(true,1),
+        };
 
-        FindButtonAndClickIt("MonetizrMenuTeaser");
 
-        yield return new WaitForSeconds(1);
+        yield return new TasksManager().Run(tasks);
 
-        FindButtonAndClickIt("RewardCenterCloseButton");
-
-        yield return new WaitForSeconds(1);
-
-        MonetizrManager.HideTinyMenuTeaser();
-
-        yield return new WaitForSeconds(1);
-
-        MonetizrManager.ShowTinyMenuTeaser();
-
-        yield return new WaitForSeconds(1);
-
-        //foreach (var v in eventsAmount)
-        //   Debug.Log($"-----------impr {v.Key} {v.Value}");
-        //while (!isTestDone)
-        //    yield return null;
-        //Assert.IsNotNull(o);
+        //---
 
         Assert.AreEqual(eventsAmount["TinyTeaser"], 4);
         Assert.AreEqual(eventsAmount["RewardsCenterScreen"], 2);
 
         //------
 
-        
-
-
-
         Debug.Log("done");
 
-        //yield return null;
+        yield return null;
     }
 
-    internal class ClickTask
-    {
-        public string button;
-        public int delay;
-
-        public ClickTask(string button, int delay)
-        {
-            this.button = button;
-            this.delay = delay;
-        }
-    }
+   
 
 
     [UnityTest]
     public IEnumerator RewardCenterItemsTest()
     {
-        while (loadSuccess == 0)
-            yield return null;
-
-        yield return new WaitForSeconds(1);
-
-
-        var tasks = new List<ClickTask>() {
+        var tasks = new List<TestUITask>() {
             new ClickTask("MonetizrMenuTeaser",2),
 
             new ClickTask("RewardCenterButtonClaim0",3),
@@ -197,31 +155,175 @@ public class Tests
             new ClickTask("PanelCloseButton",2),
             new ClickTask("MessageCloseButton",2),
             new ClickTask("NotifyCloseButton",2),
+
+            new ClickTask("RewardCenterButtonClaim4",4),
+            new ClickTask("PanelCloseButton",2),
+            new ClickTask("MessageCloseButton",2),
+            new ClickTask("NotifyCloseButton",2),
         };
 
+        yield return new TasksManager().Run(tasks);
+    }
 
-        foreach(var t in tasks)
+    [UnityTest]
+    public IEnumerator TestEmailScreen()
+    {
+        var tasks = new List<TestUITask>() {
+
+            new ClickTask("MonetizrMenuTeaser",2),
+
+            new ClickTask("RewardCenterButtonClaim4",2),
+            new InputFieldTask("PanelInputField","aa@a.a",2),
+            new TestInteractivity("PanelOkButton",false,1),
+
+            new ClickTask("PanelCloseButton",2),
+            new ClickTask("MessageCloseButton",2),
+            //new ClickTask("NotifyCloseButton",2),
+        };
+
+        yield return new TasksManager().Run(tasks);
+
+        //yield return null;
+    }
+
+    class TasksManager
+    {
+        List<TestUITask> tasks = new List<TestUITask>();
+
+        public TasksManager Add(TestUITask task)
         {
-            FindButtonAndClickIt(t.button);
+            tasks.Add(task);
+            return this;
+        }
 
-            yield return new WaitForSeconds(t.delay);
-        };
+        public TasksManager Add(List<TestUITask> tasks)
+        {
+            this.tasks.AddRange(tasks);
+            return this;
+        }
+
+        public IEnumerator Run(List<TestUITask> tasks)
+        {
+            while (Tests.loadSuccess == 0)
+                yield return null;
+
+
+            yield return new WaitForSeconds(1);
+
+            this.tasks.AddRange(tasks);
+
+            foreach (var t in tasks)
+            {
+                t.Do();
+
+                yield return new WaitForSeconds(t.delay);
+            };
+        }
 
     }
 
+    abstract class TestUITask
+    {
+        public string button;
+        public int delay;
 
-    void FindButtonAndClickIt(string name)
+        public TestUITask(string button, int delay)
+        {
+            this.button = button;
+            this.delay = delay;
+        }
+
+        abstract public void Do();
+    }
+
+    class ClickTask : TestUITask
     {        
-        var buttonObject = FindObjectByName(name);
+        public ClickTask(string button, int delay) : base(button, delay)
+        {
+ 
+        }
 
-        Assert.IsNotNull(buttonObject, $"Game object {name} is null!");
+        public override void Do()
+        {        
+            var buttonObject = Tests.FindObjectByName(button);
 
-        Button b = buttonObject.GetComponent<Button>();
+            Assert.IsNotNull(buttonObject, $"Game object {button} is null!");
 
-        Assert.IsNotNull(b,$"Button {name} is null!");
+            Button b = buttonObject.GetComponent<Button>();
 
-        b.onClick.Invoke();
+            Assert.IsTrue(b.interactable, $"Button {button} is not active!");
 
-        Debug.Log("");
+            Assert.IsNotNull(b,$"Button {button} is null!");
+
+            b.onClick.Invoke();
+        }
+    }
+
+    class TestInteractivity : TestUITask
+    {
+        bool interactive;
+
+        public TestInteractivity(string button, bool interactive, int delay) : base(button, delay)
+        {
+            this.interactive = interactive;
+        }
+
+        public override void Do()
+        {
+            var buttonObject = Tests.FindObjectByName(button);
+
+            Assert.IsNotNull(buttonObject, $"Game object {button} is null!");
+
+            Button b = buttonObject.GetComponent<Button>();
+
+            Assert.IsTrue(b.interactable == interactive, $"Button {button} is wrongly interactive. Should be {interactive}!");
+
+            Assert.IsNotNull(b, $"Button {button} is null!");
+        }
+    }
+
+    class InputFieldTask : TestUITask
+    {
+        string text;
+
+        public InputFieldTask(string button, string text, int delay) : base(button, delay)
+        {
+            this.text = text;
+        }
+
+        public override void Do()
+        {
+            var buttonObject = FindObjectByName(button);
+
+            Assert.IsNotNull(buttonObject, $"Game object {button} is null!");
+
+            InputField inf = buttonObject.GetComponent<InputField>();
+
+            Assert.IsNotNull(inf, $"Button {button} is null!");
+
+            inf.text = text;
+        }
+    }
+
+    class TeaserShowTask : TestUITask
+    {
+        bool show;
+
+        public TeaserShowTask(bool show, int delay) : base("",delay)
+        {
+            this.show = show;
+        }
+
+        public override void Do()
+        {
+            if(show)
+            {                
+                MonetizrManager.ShowTinyMenuTeaser();
+            }
+            else
+            {
+                MonetizrManager.HideTinyMenuTeaser();
+            }
+        }
     }
 }
