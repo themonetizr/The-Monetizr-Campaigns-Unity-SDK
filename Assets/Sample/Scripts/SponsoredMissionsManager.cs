b8e8c2096892c7f540a69687bc1008fd1fdb244c
akmtzr <artem@themonetizr.com>
2023-07-25 19:09:44 +0300

    Fixing DEV-846: Second reward in SDK sample

--- Assets/Sample/Scripts/SponsoredMissionsManager.cs
+++ Assets/Sample/Scripts/SponsoredMissionsManager.cs
@@ -8,30 +8,25 @@ using UnityEngine.UI;
 public class SponsoredMissionsManager : MonoBehaviour
 {
     public Sprite defaultRewardIcon;
+    public Sprite gemsRewardIcon;
     public GameObject dummyUI;
 
-    private void GetAdvertisingId(out string advertisingID, out bool limitAdvertising)
+    private static void GetAdvertisingId(out string advertisingID, out bool limitAdvertising)
     {
 #if !UNITY_EDITOR
-#if UNITY_ANDROID
-               //AndroidJavaClass versionInfo = new AndroidJavaClass("android/os/Build$VERSION");
+    #if UNITY_ANDROID
+                   AndroidJavaClass up = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");
+                   AndroidJavaObject currentActivity = up.GetStatic<AndroidJavaObject> ("currentActivity");
+                   AndroidJavaClass client = new AndroidJavaClass ("com.google.android.gms.ads.identifier.AdvertisingIdClient");
+                   AndroidJavaObject adInfo = client.CallStatic<AndroidJavaObject> ("getAdvertisingIdInfo",currentActivity);
 
-               //osVersion = versionInfo.GetStatic<string>("RELEASE");
+                   advertisingID = adInfo.Call<string> ("getId").ToString();   
+                   limitAdvertising = (adInfo.Call<bool> ("isLimitAdTrackingEnabled"));
 
-               AndroidJavaClass up = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");
-               AndroidJavaObject currentActivity = up.GetStatic<AndroidJavaObject> ("currentActivity");
-               AndroidJavaClass client = new AndroidJavaClass ("com.google.android.gms.ads.identifier.AdvertisingIdClient");
-               AndroidJavaObject adInfo = client.CallStatic<AndroidJavaObject> ("getAdvertisingIdInfo",currentActivity);
-
-               advertisingID = adInfo.Call<string> ("getId").ToString();   
-               limitAdvertising = (adInfo.Call<bool> ("isLimitAdTrackingEnabled"));
-
-#elif UNITY_IOS
-              // osVersion = UnityEngine.iOS.Device.systemVersion;
-               limitAdvertising = !(ATTrackingStatusBinding.GetAuthorizationTrackingStatus() == ATTrackingStatusBinding.AuthorizationTrackingStatus.AUTHORIZED);
-               advertisingID = Device.advertisingIdentifier;
-
-#endif
+    #elif UNITY_IOS
+                  limitAdvertising = !(ATTrackingStatusBinding.GetAuthorizationTrackingStatus() == ATTrackingStatusBinding.AuthorizationTrackingStatus.AUTHORIZED);
+                  advertisingID = Device.advertisingIdentifier;
+    #endif
 #else
         advertisingID = "";
         limitAdvertising = false;
@@ -42,17 +37,16 @@ public class SponsoredMissionsManager : MonoBehaviour
     private void Start()
     {
         //temporary API key for testing
-        var key = "t_rsNjLXzbaWkJrXdvUVEc4IW2zppWyevl9j_S5Valo";
+        const string key = "t_rsNjLXzbaWkJrXdvUVEc4IW2zppWyevl9j_S5Valo";
 
-        string advertisingID = "";
-        bool limitAdvertising = false;
+        GetAdvertisingId(out var advertisingID, out var limitAdvertising);
 
-        GetAdvertisingId(out advertisingID, out limitAdvertising);
+        //Log.isVerbose = true;
 
         MonetizrManager.SetAdvertisingIds(advertisingID, limitAdvertising);
 
         //define reward type, name, getter and adder for reward
-        MonetizrManager.SetGameCoinAsset(RewardType.Coins, defaultRewardIcon, "Coins", () =>
+        MonetizrManager.SetGameCoinAsset(RewardType.Reward1, defaultRewardIcon, "Coins", () =>
                 {
                     //return current amount of coins
                     return 0;// GameController.I.GetCoinsTotal();
@@ -62,6 +56,17 @@ public class SponsoredMissionsManager : MonoBehaviour
                     //add coins
                     //GameController.I.AddCoinsTotal(reward);
                 }, 10000);
+
+        MonetizrManager.SetGameCoinAsset(RewardType.Reward2, gemsRewardIcon, "Gems", () =>
+            {
+                //return current amount of coins
+                return 0;// GameController.I.GetCoinsTotal();
+            },
+            (ulong reward) =>
+            {
+                //add coins
+                //GameController.I.AddCoinsTotal(reward);
+            }, 100);
 
 
         //good default placement for teaser
