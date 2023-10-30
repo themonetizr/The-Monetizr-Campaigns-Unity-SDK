7c8cbd4c71feb7a74d19f43d97d025a0078cc9a3
artem <artem@themonetizr.com>
2023-10-30 16:00:13 +0200

    Fixing DEV-1409: Adding Unit Test for json parsing

--- Assets/MonetizrCampaigns/Tests/UtilsTests.cs
+++ Assets/MonetizrCampaigns/Tests/UtilsTests.cs
@@ -33,9 +33,47 @@ public class UtilsTests
         Assert.AreEqual(Utils.EncodeStringIntoAscii("Hello World"), "Hello World");
         Assert.AreEqual(Utils.EncodeStringIntoAscii("Héllø Wørld"), "H\\u00e9ll\\u00f8 W\\u00f8rld");
         Assert.AreEqual(Utils.EncodeStringIntoAscii("ÄÖÜ"), "\\u00c4\\u00d6\\u00dc");
-        
-
+     
         //Assert.AreNotEqual(Utils.ShuffleList({"1,2,3"}), 3);
     }
-    
+
+    public void PrintDictionary(Dictionary<string, string> dict)
+    {
+        foreach (KeyValuePair<string, string> entry in dict)
+        {
+            Debug.Log($"Key: {entry.Key}, Value: {entry.Value}");
+        }
+    }
+
+    [Test]
+    public void ParseJsonTest()
+    {
+        Assert.AreEqual(Utils.ParseJson(null), new Dictionary<string, string>());
+
+        var json1 = @"{
+        ""key1"": ""value1""
+        }";
+
+        var expectedResult1 = new Dictionary<string, string>
+        {
+            { "key1", "value1" },
+        };
+
+        Assert.AreEqual(Utils.ParseJson(json1), expectedResult1);
+
+        var json2 = @"{
+        ""key1"": 0,
+        ""key2"": ""value2""
+        }";
+
+        var expectedResult2 = new Dictionary<string, string>
+        {
+            { "key1", "0" },
+            { "key2", "value2" },
+        };
+                
+        Assert.AreEqual(Utils.ParseJson(json2), expectedResult2);
+    }
+
+
 }
