6f1dcc6a869d8edce2822dfc4b705825ca2d370f
Gabriel Menezes <38543018+gpdmenezes@users.noreply.github.com>
2024-07-10 09:41:51 -0300

    Reward Center Video Timeout Fix

--- Assets/MonetizrCampaigns/Scripts/Core/MonetizrSDKConfiguration.cs
+++ Assets/MonetizrCampaigns/Scripts/Core/MonetizrSDKConfiguration.cs
@@ -2,7 +2,7 @@ namespace Monetizr.SDK.Core
 {
     public static class MonetizrSDKConfiguration
     {
-        public static readonly string SDKVersion = "1.0.8";
+        public static readonly string SDKVersion = "1.0.7.1";
         public static float requestCampaignTime = 300f;
     }
 }
